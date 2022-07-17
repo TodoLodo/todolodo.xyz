9f15f1779422e07b9b692680051bc1d047171ed7
TodoLodo2089 <me@todolodo.xyz>
2022-07-17 23:08:26 +0530

    Update, date - Sun 07/17/2022, time - 23:08:26.53

--- static/js/common.js
+++ static/js/common.js
@@ -1,5 +1,27 @@
 window.scrollTo({top: 0, behavior: 'smooth'});
 
+// --------- Facebook SDK ----------------------------
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '{your-app-id}',
+    cookie     : true,
+    xfbml      : true,
+    version    : '{api-version}'
+  });
+    
+  FB.AppEvents.logPageView();   
+    
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+// --------------------------------------------------
+
 var todo = document.getElementById("todo");
 var main = document.getElementById("main");
 var scrollbarContainer = document.getElementById("scrollbar-container");
