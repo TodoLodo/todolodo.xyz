459a918cc621118e94645e2741e72deccac8a69f
TodoLodo2089 <me@todolodo.xyz>
2022-07-18 21:29:04 +0530

    Update, date - Mon 07/18/2022, time - 21:29:04.54

--- static/js/cod-python-api/common.js
+++ static/js/cod-python-api/common.js
@@ -1,3 +1,9 @@
+let path = document.querySelector("#landing-path");
+const length = path.getTotalLength();
+
+path.style.strokeDasharray = length;
+path.style.strokeDashoffset = length; 
+
 let navList = document.getElementsByClassName('nav');
 var style = window.getComputedStyle(navList[0], null).getPropertyValue('font-size');
 var fontSize = parseFloat(style); 
--- static/js/cod-python-api/landing.js
+++
@@ -1,5 +0,0 @@
-let path = document.querySelector("#landing-path");
-const length = path.getTotalLength();
-
-path.style.strokeDasharray = length;
-path.style.strokeDashoffset = length; 
